c35ec83efc68a72e454c94c258aa784cbbfb43aa
Elena Kvasko <lenakvasko@gmail.com>
2023-07-04 19:39:19 +0300

    Added files

---
+++ controllers/contacts/removeContact.js
@@ -0,0 +1,15 @@
+const { Contact } = require("../../models/contact");
+const { HttpError } = require("../../helpers");
+
+const removeContact = async (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "contact deleted" });
+};
+
+module.exports = removeContact;
